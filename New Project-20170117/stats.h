aed433c6d31d6c4dfddb6181e7f50750d921de45
Artem Yazykov <yazikov87@gmail.com>
2017-01-17 16:51:16 +0300

    Created by Coding Ground Client

---
+++ New Project-20170117/stats.h
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <vector>
+
+double smallest(const std::vector<double>& vec_in);
+double greatest(const std::vector<double>& vec_in);
+double mean(const std::vector<double>& vec_in);
+double total(const std::vector<double>& vec_in);
